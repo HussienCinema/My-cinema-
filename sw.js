93c7be3b391b26f92d9d6dd90cd09a4230907b1e
HussienCinema <tv.hussien.tv@gmail.com>
2026-01-16 03:57:30 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,9 @@
 self.addEventListener('install', (event) => {
     self.skipWaiting();
 });
-
 self.addEventListener('activate', (event) => {
     return self.clients.claim();
 });
-
 self.addEventListener('fetch', (event) => {
     // ملف فارغ لغرض تفعيل التثبيت فقط
 });
